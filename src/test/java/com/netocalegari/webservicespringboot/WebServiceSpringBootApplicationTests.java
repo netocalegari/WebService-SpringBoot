0500fff6178f48f94327b10eaf4847363ee88aa8
netocalegari <netokalegari@gmail.com>
2024-03-17 21:20:35 -0300

    Project creation

---
+++ src/test/java/com/netocalegari/webservicespringboot/WebServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netocalegari.webservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
