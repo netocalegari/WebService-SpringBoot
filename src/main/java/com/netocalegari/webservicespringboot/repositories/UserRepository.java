9992d0dbe7a56bcfe77e97e43e29e601decbef06
netocalegari <netokalegari@gmail.com>
2024-03-18 16:03:28 -0300

    Service layer, component registration

--- src/main/java/com/netocalegari/webservicespringboot/repositories/UserRepository.java
+++ src/main/java/com/netocalegari/webservicespringboot/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.netocalegari.webservicespringboot.repositories;
 import com.netocalegari.webservicespringboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/netocalegari/webservicespringboot/resources/UserResource.java
+++ src/main/java/com/netocalegari/webservicespringboot/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.netocalegari.webservicespringboot.resources;
 
 import com.netocalegari.webservicespringboot.entities.User;
+import com.netocalegari.webservicespringboot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Neto", "neto@gmail.com", "99999999", "12345");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
 
-        return ResponseEntity.ok().body(user);
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
