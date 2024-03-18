d10626f427896f2a2704fd33a955ba3de69d36dc
netocalegari <netokalegari@gmail.com>
2024-03-18 15:11:07 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/netocalegari/webservicespringboot/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.netocalegari.webservicespringboot.repositories;
+
+import com.netocalegari.webservicespringboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
