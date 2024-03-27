c396c5810bdd4e3f12e08023aaa4bd3e5912a097
netocalegari <netokalegari@gmail.com>
2024-03-27 14:26:00 -0300

    User delete

--- src/main/java/com/netocalegari/webservicespringboot/resources/UserResource.java
+++ src/main/java/com/netocalegari/webservicespringboot/resources/UserResource.java
@@ -28,7 +28,7 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj) {
         service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -36,4 +36,10 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
