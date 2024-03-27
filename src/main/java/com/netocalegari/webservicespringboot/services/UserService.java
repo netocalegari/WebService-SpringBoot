5e31d64a6f79bacfc6fde8a77d8d78a9ceaf4e8a
netocalegari <netokalegari@gmail.com>
2024-03-27 15:00:22 -0300

    Exception handling - findById

--- src/main/java/com/netocalegari/webservicespringboot/services/UserService.java
+++ src/main/java/com/netocalegari/webservicespringboot/services/UserService.java
@@ -2,6 +2,7 @@ package com.netocalegari.webservicespringboot.services;
 
 import com.netocalegari.webservicespringboot.entities.User;
 import com.netocalegari.webservicespringboot.repositories.UserRepository;
+import com.netocalegari.webservicespringboot.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
